1045da05670702809ecac61b510c64aa3d99a9a2
LgFvm9353 <2790928695@qq.com>
2026-01-01 22:09:37 +0800

    fix api base url for server

--- novel_backened/src/main/java/org/example/novel_backened/config/WebConfig.java
+++ novel_backened/src/main/java/org/example/novel_backened/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://47.94.110.108:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- novel_fronted/src/api/http.js
+++ novel_fronted/src/api/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/auth'
 
 const http = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://47.94.110.108:8080/api',
   timeout: 10000,
 })
 
